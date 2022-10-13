2eaca773e1cdd111ad4a812e8441364fb8aaab72
Sarvar55 <sarvar.musazade@gmail.com>
2022-10-13 22:02:30 +0300

    pages updated

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -4,11 +4,20 @@ var router = express.Router();
 const anaSayfa = function(req, res, next) {
     res.render("anasayfa", { title: "Ansayfa" });
 };
-const mekanBilgisi = (req, res, next) => {
-    res.render("mekanbilgisi", { title: "Mekan Bilgisi" });
-};
+// const mekanBilgisi = (req, res, next) => {
+//     res.render("mekanbilgisi", { title: "Mekan Bilgisi" });
+// };
 const yorumEkle = (req, res, next) => {
     res.render("yorumekle", { title: "Yorum ekle" });
+};
+const mekanBilgisi = function(req, res) {
+    const generateAddress = {
+        "Starbucks": " İyaş Market Girişi",
+        "Arabica": "İyaş Avm Girişi",
+        "İyaş": "Otogar Karşısı"
+    }
+    console.log(generateAddress[req.params.title])
+    res.render('mekanbilgisi', { title: req.params.title, address: generateAddress[req.params.title] });
 };
 
 module.exports = {
