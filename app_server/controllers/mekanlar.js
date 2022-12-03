c4544458c84a8b27cec7719b3d6b41d9d9802e90
Sarvar55 <sarvar.musazade@gmail.com>
2022-12-03 16:20:41 +0300

    hata giderildi

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,14 +1,14 @@
 var express = require("express");
-var router = express.Router();
 const axios = require("axios");
 
-const apiSecenekleri = {
+var apiSecenekleri = {
+    // sunucu: "http://localhost:3000",
     sunucu: "https://mekanbul.servermusazade.repl.co",
     apiYolu: "/api/mekanlar/",
 };
 
 const mesafeyiFormatla = ({ mesafe }) => {
-    let yeniMesafe, birim;
+    var yeniMesafe, birim;
     if (mesafe > 1) {
         yeniMesafe = parseFloat(mesafe).toFixed(1);
         birim = " km";
@@ -16,56 +16,30 @@ const mesafeyiFormatla = ({ mesafe }) => {
         yeniMesafe = parseInt(mesafe * 1000, 10);
         birim = " m";
     }
-
     return yeniMesafe.concat(birim);
 };
-
 const anaSayfaOlustur = (res, mekanListesi) => {
     let mesaj;
     if (!(mekanListesi instanceof Array)) {
-        mesaj = "API Hatasi:Bir seyler ters gitdi";
+        mesaj = "API HATASI: Bir şeyler ters gitti.";
         mekanListesi = [];
     } else {
         if (!mekanListesi.length) {
-            mesaj = "Civarda herhengi bir mekan yok";
+            mesaj = "Civarda herhangi bir mekan yok";
         }
     }
-    res.render("anaSayfa", {
+    res.render("anasayfa", {
         baslik: "Anasayfa",
         sayfaBaslik: {
             siteAd: "MekanBul",
-            slogan: "Civardaki Makenlari kesfet",
+            slogan: "Civardaki Mekanları Keşfet!",
         },
         mekanlar: mekanListesi,
         mesaj,
     });
 };
 
-const anaSayfa = function(req, res) {
-    const { sunucu, apiYolu } = apiSecenekleri;
-    axios
-        .get(sunucu.concat(apiYolu), {
-            params: {
-                enlem: req.query.enlem,
-                boylam: req.query.boylam,
-            },
-        })
-        .then((response) => {
-            mekanlar = response.data.map((mekan) => {
-                mesafe = mesafeyiFormatla(mekan);
-                return {
-                    ...mekan,
-                    mesafe,
-                };
-            });
-            anaSayfaOlustur(res, mekanlar);
-        })
-        .catch((error) => {
-            anaSayfaOlustur(res, error);
-            console.log(JSON.stringify(error));
-        });
-};
-const detaySayfasiOlustur = (res, mekanDetaylari) => {
+var detaySayfasiOlustur = (res, mekanDetaylari) => {
     mekanDetaylari.koordinat = {
         enlem: mekanDetaylari.koordinat[0],
         boylam: mekanDetaylari.koordinat[1],
@@ -76,6 +50,42 @@ const detaySayfasiOlustur = (res, mekanDetaylari) => {
     });
 };
 
+const hataGoster = (res, hata) => {
+    let mesaj;
+    if (hata.response.status == 404) mesaj = "404, Sayfa Bulunamadı!";
+    else mesaj = hata.response.status + " hatası";
+
+    res.status(hata.response.status);
+    res.render("error", {
+        mesaj: mesaj,
+    });
+};
+
+const anaSayfa = (req, res) => {
+    const { sunucu, apiYolu } = apiSecenekleri;
+    axios
+        .get(sunucu.concat(apiYolu), {
+            params: {
+                enlem: req.query.enlem,
+                boylam: req.query.boylam,
+            },
+        })
+        .then((response) => {
+            let mesafe, mekanlar;
+            mekanlar = response.data.map((mekan) => {
+                mesafe = mesafeyiFormatla(mekan);
+                return {
+                    ...mekan,
+                    mesafe,
+                };
+            });
+            anaSayfaOlustur(res, mekanlar);
+        })
+        .catch((hata) => {
+            anaSayfaOlustur(res, hata);
+        });
+};
+
 const mekanBilgisi = (req, res) => {
     const { sunucu, apiYolu } = apiSecenekleri;
     const { mekanid } = req.params;
@@ -84,21 +94,13 @@ const mekanBilgisi = (req, res) => {
         .then((response) => {
             detaySayfasiOlustur(res, response.data);
         })
-        .catch((error) => {
-            hataGoster(res, error);
+        .catch((hata) => {
+            hataGoster(res, hata);
         });
 };
-const yorumEkle = (req, res, next) => {
-    res.render("yorumekle", { title: "Yorum ekle" });
-};
-const hataGoster = (res, hata) => {
-    let mesaj;
-    if (hata.response.status == 404) mesaj = "404 Sayfa bulunamadi";
-    else mesaj = hata.response.status + "hatasi";
-    res.status(hata.response.status);
-    res.render("error", {
-        mesaj,
-    });
+
+const yorumEkle = function(req, res, next) {
+    res.render("yorumekle", { title: "Yorum Ekle" });
 };
 
 module.exports = {
