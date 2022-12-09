5ef6c0c75f6efc481e2344deda0cba0bf384df0d
Sarvar55 <sarvar.musazade@gmail.com>
2022-12-09 23:12:04 +0300

    odev8 tamamlandi

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,27 +1,24 @@
-var express = require("express");
 const axios = require("axios");
-
 var apiSecenekleri = {
-    //sunucu: "http://localhost:3000",
-    sunucu: "https://mekanbul.servermusazade.repl.co",
+    //sunucu:"http://localhost:3000",
+    sunucu: "https://mekanbul.servermusazade.repl.co/",
     apiYolu: "/api/mekanlar/",
 };
-
-const mesafeyiFormatla = ({ mesafe }) => {
+var mesafeyiFormatla = function(mesafe) {
     var yeniMesafe, birim;
     if (mesafe > 1) {
         yeniMesafe = parseFloat(mesafe).toFixed(1);
         birim = " km";
     } else {
-        yeniMesafe = parseInt(mesafe * 1000, 10);
+        yeniMesafe = parseInt(mesafe * 1000, 10); //onluk düzende olduğunu ifade eder....
         birim = " m";
     }
-    return yeniMesafe.concat(birim);
+    return yeniMesafe + birim;
 };
-const anaSayfaOlustur = (res, mekanListesi) => {
-    let mesaj;
+var anaSayfaOlustur = function(res, mekanListesi) {
+    var mesaj;
     if (!(mekanListesi instanceof Array)) {
-        mesaj = "API HATASI: Bir şeyler ters gitti.";
+        mesaj = "API HATASI:Bir şeyler ters gitti.";
         mekanListesi = [];
     } else {
         if (!mekanListesi.length) {
@@ -32,113 +29,106 @@ const anaSayfaOlustur = (res, mekanListesi) => {
         baslik: "Anasayfa",
         sayfaBaslik: {
             siteAd: "MekanBul",
-            slogan: "Civardaki Mekanları Keşfet!",
+            slogan: "Mekanları Keşfet",
         },
         mekanlar: mekanListesi,
-        mesaj,
-    });
-};
-
-var detaySayfasiOlustur = (res, mekanDetaylari) => {
-    mekanDetaylari.koordinat = {
-        enlem: mekanDetaylari.koordinat[0],
-        boylam: mekanDetaylari.koordinat[1],
-    };
-    res.render("mekanbilgisi", {
-        mekanBaslik: mekanDetaylari.ad,
-        mekanDetay: mekanDetaylari,
-    });
-};
-
-const hataGoster = (res, hata) => {
-    let mesaj;
-    if (hata.response.status == 404) mesaj = "404, Sayfa Bulunamadı!";
-    else mesaj = hata.response.status + " hatası";
-
-    res.status(hata.response.status);
-    res.render("error", {
         mesaj: mesaj,
     });
 };
-
-const anaSayfa = (req, res) => {
-    const { sunucu, apiYolu } = apiSecenekleri;
+const anaSayfa = function(req, res) {
     axios
-        .get(sunucu.concat(apiYolu), {
+        .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu, {
             params: {
                 enlem: req.query.enlem,
                 boylam: req.query.boylam,
             },
         })
-        .then((response) => {
-            let mesafe, mekanlar;
-            mekanlar = response.data.map((mekan) => {
-                mesafe = mesafeyiFormatla(mekan);
-                return {
-                    ...mekan,
-                    mesafe,
-                };
-            });
+        .then(function(response) {
+            var i, mekanlar;
+            mekanlar = response.data;
+            for (i = 0; i < mekanlar.length; i++) {
+                mekanlar[i].mesafe = mesafeyiFormatla(mekanlar[i].mesafe);
+            }
             anaSayfaOlustur(res, mekanlar);
         })
-        .catch((hata) => {
+        .catch(function(hata) {
             anaSayfaOlustur(res, hata);
         });
 };
+var detaySayfasiOlustur = function(res, mekanDetaylari) {
+    mekanDetaylari.koordinat = {
+        enlem: mekanDetaylari.koordinat[0],
+        boylam: mekanDetaylari.koordinat[1],
+    };
+    res.render("mekanbilgisi", {
+        baslik: "MekanBilgisi",
+        mekanBaslik: mekanDetaylari.ad,
+        mekanDetay: mekanDetaylari,
+    });
+};
+var hataGoster = function(res, hata) {
+    var mesaj;
+    if (hata.response.status == 404) {
+        mesaj = "404, Sayfa Bulunamadı!";
+    } else {
+        mesaj = hata.response.status + " hatası";
+    }
+    res.status(hata.response.status);
+    res.render("error", {
+        mesaj,
+    });
+};
 
-const mekanBilgisi = (req, res) => {
-    const { sunucu, apiYolu } = apiSecenekleri;
-    const { mekanid } = req.params;
+const mekanBilgisi = function(req, res) {
     axios
-        .get(sunucu.concat(apiYolu) + mekanid)
-        .then((response) => {
+        .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+        .then(function(response) {
             req.session.mekanAdi = response.data.ad;
             detaySayfasiOlustur(res, response.data);
         })
-        .catch((hata) => {
+        .catch(function(hata) {
             hataGoster(res, hata);
         });
 };
 
-const yorumEkle = function(req, res, next) {
-    let mekanAdi = req.session.mekanAdi;
-    let mekanid = req.params.mekanid;
+const yorumEkle = function(req, res) {
+    var mekanAdi = req.session.mekanAdi;
+    mekanid = req.params.mekanid;
     if (!mekanAdi) {
         res.redirect("/mekan/" + mekanid);
     } else {
         res.render("yorumekle", {
-            baslik: mekanAdi + " mekanina yorum Ekle",
+            baslik: mekanAdi + " mekanına yorum ekle",
         });
     }
 };
-
-const yorummuEkle = (req, res) => {
-    let gonderilenYorum;
-    const { mekanid } = req.params;
+const yorumumuEkle = function(req, res) {
+    var gonderilenYorum, mekanid;
+    mekanid = req.params.mekanid;
     if (!req.body.adsoyad || !req.body.yorum) {
         res.redirect("/mekan/" + mekanid + "/yorum/yeni?hata=evet");
     } else {
-        const { adsoyad, puan, yorum } = req.body;
         gonderilenYorum = {
-            yorumYapan: adsoyad,
-            puan,
-            yorumMetni: yorum,
+            yorumYapan: req.body.adsoyad,
+            puan: parseInt(req.body.puan, 10),
+            yorumMetni: req.body.yorum,
         };
-        const { sunucu, apiYolu } = apiSecenekleri;
         axios
-            .post(`${sunucu}${apiYolu}${mekanid}/yorumlar`, gonderilenYorum)
-            .then((response) => {
-                res.redirect("/mekan/".concat(mekanid));
+            .post(
+                apiSecenekleri.sunucu + apiSecenekleri.apiYolu + mekanid + "/yorumlar",
+                gonderilenYorum
+            )
+            .then(function() {
+                res.redirect("/mekan/" + mekanid);
             })
-            .catch((err) => {
-                hataGoster(req, res, err);
+            .catch(function(hata) {
+                hataGoster(req, res, hata);
             });
     }
 };
-
 module.exports = {
     anaSayfa,
     mekanBilgisi,
     yorumEkle,
-    yorummuEkle,
+    yorumumuEkle,
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -7,7 +7,7 @@ var ctrlDigerleri = require("../controllers/digerleri");
 router.get("/", ctrlMekanlar.anaSayfa);
 router.get("/mekan/:mekanid", ctrlMekanlar.mekanBilgisi);
 router.get("/mekan/:mekanid/yorum/yeni", ctrlMekanlar.yorumEkle);
-router.post("/mekan/:mekanid/yorum/yeni", ctrlMekanlar.yorummuEkle);
+router.post("/mekan/:mekanid/yorum/yeni", ctrlMekanlar.yorumumuEkle);
 router.get("/hakkinda", ctrlDigerleri.hakkinda);
 
 module.exports = router;
