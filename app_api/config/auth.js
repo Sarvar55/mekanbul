b3bedd0b31b1be966cdd56989a88f09f9bb1a427
Sarvar55 <sarvar.musazade@gmail.com>
2023-05-18 16:45:54 +0300

    messaje

--- app_api/config/auth.js
+++ app_api/config/auth.js
@@ -4,24 +4,23 @@ const router = experss.Router();
 const jsonwebtoken = require("jsonwebtoken");
 
 const getTokenFromHeaders = (req) => {
-    const {
-        headers: { authorization },
-    } = req;
+  const {
+    headers: { authorization },
+  } = req;
 
-    if (authorization && authorization.split(" ")[0] === "Bearer")
-        return authorization.split(" ")[1];
+  if (authorization && authorization.split(" ")[0] === "Bearer")
+    return authorization.split(" ")[1];
 
-    return null;
+  return null;
 };
 
-
 const auth = jwt.expressjwt({
-    secret: process.env.SECRET_KEY,
-    userPorperty: "payload",
-    algorithms: ["sha1", "RSA256", "HS256"],
-    getToken: getTokenFromHeaders,
+  secret: process.env.SECRET_KEY,
+  userPorperty: "payload",
+  algorithms: ["sha1", "RSA256", "HS256"],
+  getToken: getTokenFromHeaders,
 });
 
 module.exports = {
-    auth,
+  auth,
 };
